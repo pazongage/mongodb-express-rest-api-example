55aef767d5b4477bf5a9f210348a8f6c163079e4
Paz Fridman <pazfrid@gmail.com>
2024-10-27 11:29:33 +0000

    Upload files

---
+++ app/src/config.js
@@ -0,0 +1,5 @@
+// export const baseUrl = "http://localhost:5050";
+// export const baseUrl = "http://129.159.157.200:5050";
+// export const baseUrl = "http://129.159.157.200/server";
+export const baseUrl = "http://server:5050";
+// export const baseUrl = "http://server";
